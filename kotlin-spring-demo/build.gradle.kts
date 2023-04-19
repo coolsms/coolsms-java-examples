471d342722d2514680e623fe0f59c24d99f7b5c7
hosy <hosy@nurigo.net>
2023-04-19 12:05:26 +0900

    Update build.gradle.kts

--- kotlin-spring-demo/build.gradle.kts
+++ kotlin-spring-demo/build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 
-	implementation("net.nurigo:sdk:4.2.7")
+	implementation("net.nurigo:sdk:4.3.0")
 }
 
 tasks.withType<KotlinCompile> {
